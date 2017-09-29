781ec89eb88305af64e3f7aeca2cd2f0e35abe15
Nanyun Peng <nanyunpe@hpc-login3.hpcc.usc.edu>
2017-09-28 23:43:56 -0700

    update the pre-process script. start run training on lookback-1 data

--- preprocess.sh
+++ preprocess.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # tokenize and generate .query, .reply and .topic files.
-python ./preprocess/process_vallina.py $1 $2 $3 || printf 'Usage: ./preprocess.sh input_dir, output_dir file_name'; exit
+printf 'tokenize and generate .query, .reply and .topic files.\n'
+python ./preprocess/process_vallina.py $1 $2 $3 || (printf 'Usage: ./preprocess.sh input_dir, output_dir file_name'; exit)
 
 # generate dictionary
-python ./preprocess/build_dictionary.py $3 || exit
+printf 'Generating dictionary!\n'
+python ./preprocess/build_dictionary.py $1/$3 || exit
+
+mv $1/${3}.pkl ${2}/${3}
